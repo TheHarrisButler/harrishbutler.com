31c69fb90b250db0ac4f7f3cc3483608a942b527
Harris Butler <harrishbutler1@gmail.com>
2023-06-11 16:41:47 -0500

    wip

--- src/layout/header-menu.tsx
+++ src/layout/header-menu.tsx
@@ -1,26 +1,26 @@
 // Components
-import { Link } from '../ui-components';
+import { Link } from "../ui-components";
 // Third Party
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
 
 // Utils
-import { routes } from '../utils';
+import { routes } from "../utils";
 
 export const HeaderMenu = () => {
   const styles = {
     container: css({
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '20rem',
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: "20rem",
     }),
   };
 
   return (
     <div css={styles.container}>
-      <Link label={'About'} href={routes.about()} />
-      <Link label={'Blog'} href={routes.blog()} />
-      <Link label={'Contact'} href={routes.contact()} />
+      <Link label={"Projects"} href={routes.about()} />
+      <Link label={"Substack"} href={routes.blog()} />
+      <Link label={"Get in touch"} href={routes.contact()} />
     </div>
   );
 };
---
+++ src/pages/substack.tsx
@@ -0,0 +1,15 @@
+// Third Party
+import { NextPage } from "next";
+
+// Components
+import { Layout } from "../layout";
+
+export const Substack: NextPage = () => {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  );
+};
+
+export default Substack;
