5ab36e6e29468e75f40d8ff4dc8640c26c2ccc0c
Harris Butler <harrishbutler1@gmail.com>
2023-11-05 14:44:50 -0600

    update intro text

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -17,15 +17,12 @@ export const copyText = {
     whereItStarted: "I was born and raised in Denver, Colorado.",
     introduction:
       "After graduating from high school, I moved to Texas to play on the men's golf team at the  University of Texas at Tyler, where I graduated with a degree in Computer Science in 2021.",
-    paragraph1: `At a young age, my pursuits in computing were primarily to show off to
-       my group of friends or the coding club at school, from building little
-       hello world apps in Java to writing bash scripts to change your
-       friend's menu bar icons on their laptop to pictures of Nicolas Cage.
-       It certainly wasn't groundbreaking or even that interesting, but it
-       felt like I was learning a superpower.`,
-    paragraph2: `It wasnt until later in high school and early college that I
-       discovered the world of web and mobile development. For the first
-       time, my little side hobby felt like it could produce something real,
-       something people would actually use!`,
+    paragraph1: `My journey as a software engineer started in junior high school, where I first dipped my toes into the world of programming. 
+    What truly ignited my passion was the concept of permission-less, creative building—finding solutions to problems without constraints. 
+    This love for unconstrained innovation has been my guiding light throughout my career.`,
+    paragraph2: `I take pleasure in crafting software across various domains, but I have the most experience in developing 
+    JavaScript web applications. From front-end to back-end, I've honed my skills in creating dynamic, 
+    user-friendly experiences. My journey as a software engineer continues to unfold, driven by my unwavering 
+    enthusiasm for problem-solving and the endless opportunities in this ever-evolving field.`,
   },
 };
